031598c4e04a996670ead1a924b3c4d37d3d8987
andreantama <andreanyogatama1@gmail.com>
2024-07-24 11:07:38 +0700

    install leaflet-icon-pulse

--- resources/views/openstreetmap.blade.php
+++ resources/views/openstreetmap.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Njajal Leaflet</title>
     {{-- <link rel="stylesheet" href="{{ asset('node_modules/leaflet/dist/leaflet.css') }}"/> --}}
-     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
      crossorigin=""/>
+    <link rel="stylesheet" href="{{ asset('node_modules/@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.css') }}">
     <style>
         #map { width:100%; height: 400px; }
     </style>
@@ -24,18 +25,21 @@
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
+<script src="{{ asset('node_modules/@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.js') }}"></script>
 {{-- <script src="{{ asset('node_modules/leaflet-highlightable-layers/dist/L.HighlightableLayers.js') }}"></script> --}}
 {{-- "leaflet-highlightable-layers": "https://esm.sh/leaflet-highlightable-layers" --}}
 <script type="importmap">
 	{
 		"imports": {
 			"leaflet": "https://esm.sh/leaflet",
-			"leaflet-highlightable-layers": "{{ asset('node_modules/leaflet-highlightable-layers/dist/L.HighlightableLayers.js') }}"
+			"leaflet-highlightable-layers": "{{ asset('node_modules/leaflet-highlightable-layers/dist/L.HighlightableLayers.js') }}",
+            "@ansur/leaflet-pulse-icon": "{{ asset('node_modules/@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.js') }}"
 		}
 	}
 </script>
 <script type="module">
 	import L from "leaflet";
+    import "@ansur/leaflet-pulse-icon";
 	import { HighlightablePolyline } from "leaflet-highlightable-layers";
 
 	const map = L.map('map', { minZoom: 0, });
@@ -44,6 +48,9 @@
 		maxNativeZoom:19,
         maxZoom:25
 	}).addTo(map);
+
+    var pulsingIcon = L.icon.pulse({iconSize:[20,20],color:'red'});
+    var marker = L.marker([-7.267698877124566, 112.7588251233101],{icon: pulsingIcon}).addTo(map);
 
     map.setView([-7.26764, 112.75861], 19);
 
